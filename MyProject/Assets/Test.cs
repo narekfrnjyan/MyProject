4268c96da99622fc46077430dab85c1d6add2c10
narekfrnjyan <narekfrnjyan2005@gmail.com>
2023-10-30 16:39:48 +0400

    first

---
+++ MyProject/Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Dll.Load();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+internal static class Dll
+{
+    [DllImport("ConsoleApplication2.dll")]
+    internal static extern int Load();
+}
